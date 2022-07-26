7527b9bef95d2a7681571ee3f89aabd44c44b143
Lily <ms.lilichka@yandex.ru>
2022-07-26 15:31:10 +0500

    Seminar1 - school

---
+++ SeminarBDFerst/Work/Program.cs
@@ -0,0 +1,21 @@
+﻿//Скрипт
+string result = string.Empty;
+string[] marks = { "отличник", "ударник", "троечик" };
+for (int i = 0; i < 10; i++)
+{
+    string ferstName = $"Имя_{i}";
+    string lastName = $"Имя_{i}";
+    int id = i;
+    string mark = marks[new Random().Next(marks.Length)];
+    result += $"{id}\t{ferstName}\t{lastName}\t{mark};\n";
+}
+
+//Console.WriteLine($"{result}");
+File.WriteAllText("table.xls", result, System.Text.Encoding.UTF32);
+
+/* Псевдокод:
+   Model-ученики
+    id
+    ferstName
+    lastName
+    mark */
